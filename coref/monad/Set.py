aaee05b1de7ac59c1b872f5fb805e127d50e3fd4
vulogov <vladimir.ulogov@me.com>
2020-05-08 15:46:38 -0700

    Set Monad

---
+++ coref/monad/Set.py
@@ -0,0 +1,77 @@
+from pymonad.Monad import *
+from pymonad.Monoid import *
+
+class Set(set, Monad, Monoid):
+	"""
+	Represents a non-deterministic calculation or a calculation with more than one possible result.
+	Based on Python's built-in 'set' type, 'Set' supports most basic set operations
+	"""
+
+	def __init__(self, *values):
+		""" Takes any number of values (including none) and puts them in the Set monad. """
+		super(Set, self).__init__(values)
+
+	def __eq__(self, other):
+		if not isinstance(other, List):
+			raise TypeError("Can't compare Set with non-Set type.")
+		return super(Set, self).__eq__(other)
+
+	def __ne__(self, other):
+		if not isinstance(other, Set):
+			return True
+		return super(Set, self).__ne__(other)
+
+	@classmethod
+	def unit(cls, value):
+		return Set(value)
+
+	def getValue(self):
+		"""
+		Returns the set.
+		This method is mainly to maintain compatibility with other monads,
+		it's not strictly necessary, you can simply operate on the 'Set' like
+		any other sets in Python.
+
+		"""
+		return self
+
+	def fmap(self, function):
+		""" Applies 'function' to every element in a Set, returning a new Set. """
+		return Set(*list(map(function, self)))
+
+	def amap(self, functorValue):
+		""" Applies the function(s) stored in the functor to the contents of the 'functorValue' Set. """
+		result = set()
+		for func in self.getValue():
+			result.add(func * functorValue)
+		return Set(*result)
+
+	def bind(self, function):
+		"""
+		Applies 'function' to the result of a previous Set operation.
+		'function' should accept a single non-Set argument and return a new Set.
+		"""
+		result = set()
+		for subList in (map(function, self)):
+			result.add(subList)
+		return Set(*result)
+
+	def __rmul__(self, function):
+		return self.fmap(function)
+
+	@staticmethod
+	def mzero():
+		""" Returns the identity element (an empty Set) of the Set monoid.  """
+		return Set()
+
+	def mplus(self, other):
+		"""
+		Combines two Set monoid values into a single Set monoid by concatenating the
+		two sets together.
+
+		"""
+		return Set(*(super(Set, self).union(other)))
+
+	def __add__(self, other):
+		""" Overrides Python's native set __add__ operator to call 'mplus'.  """
+		return self.mplus(other)
--- tests/test_monad.py
+++ tests/test_monad.py
@@ -2,7 +2,8 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from coref.monad.Dict import *
+from coref.monad import *
+
 
 def test_dict_1():
     d = Dict()
@@ -22,3 +23,28 @@ def test_dict_3():
     d = Dict(answer=21)
     d = d.fmap(doubler)
     assert d['answer'] == 42
+
+def test_set_1():
+    s = Set()
+    s += Set(1,2,3)
+    assert len(s) == 3
+
+def test_set_2():
+    def echo(d):
+        return d
+    s = Set(42)
+    s.bind(echo)
+    assert 42 in s
+
+def test_set_3():
+    def doubler(x):
+        return x*2
+    s = Set(21)
+    s = s.fmap(doubler)
+    assert 42 in s
+
+def test_set_4():
+    def doubler(x):
+        return x*2
+    s = Set(21) >> doubler
+    assert 42 in s
